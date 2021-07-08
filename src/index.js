4422f9f0b9a34c3b9cc61b83fc8169da22617392
Valentine <psp32@ukr.net>
2021-07-08 17:02:43 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/app/";
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//
+ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//!Теория мини приложение с кнопкой и инпутом
+// const Header = () => {
+//   <h1> Hello World</h1>;
+// };
+
+// const Field = () => {
+//   const holder = "Enter here";
+//   const styledField = {
+//     width: "300px",
+//   };
+
+//   return (
+//     <input
+//       style={styledField}
+//       type="text"
+//       placeholder={holder}
+//       autoComplete=""
+//       className="first"
+//     />
+//   );
+// };
+
+// const Btn = () => {
+//   const text = " Log in";
+//   const logged = true;
+//   return <button>{logged ? "Enter" : text} </button>;
+// };
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <Field />
+//       <Btn />
+//     </div>
+//   );
+// };
